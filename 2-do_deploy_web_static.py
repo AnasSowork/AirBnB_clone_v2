51b1cd9fde21ad604584c29b017383005e3edd3f
Anas <anas.001falaki@gmail.com>
2024-03-07 16:14:36 +0100

    Bash - full deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,36 +1,52 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to the web servers
+Fabric script to distribute an archive to web servers
 """
-from fabric.api import env, run, put
+from fabric.api import env, put, run
 from os.path import exists
 
-env.hosts = ['52.201.211.42', '54.89.194.2']
+env.hosts = ['52.201.211.42', '54.89.194.2']  # Replace with actual IP addresses
 
 
-def do_deploy(archive_path, ssh_key, username):
+def do_deploy(archive_path):
     """
-    Distributes an archive to the web servers
+    Distribute an archive to web servers
     """
-    env.key_filename = ssh_key
-    env.user = username
-
     if not exists(archive_path):
         return False
 
     try:
-        filename = archive_path.split('/')[-1]
-        folder_name = "/data/web_static/releases/" + filename.split('.')[0]
+        # Upload the archive to /tmp/ directory of the web servers
+        put(archive_path, '/tmp/')
 
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(folder_name))
-        run("tar -xzf /tmp/{} -C {}".format(filename, folder_name))
-        run("rm /tmp/{}".format(filename))
-        run("mv {}/web_static/* {}".format(folder_name, folder_name))
-        run("rm -rf {}/web_static".format(folder_name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(folder_name))
-        print("New version deployed!")
+        # Get the filename without extension
+        file_name = archive_path.split('/')[-1].split('.')[0]
+
+        # Create the directory to uncompress the archive
+        run('mkdir -p /data/web_static/releases/{}/'.format(file_name))
+
+        # Uncompress the archive
+        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'
+            .format(file_name, file_name))
+
+        # Remove the archive from the web server
+        run('rm /tmp/{}.tgz'.format(file_name))
+
+        # Move the uncompressed files to proper location
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.format(file_name, file_name))
+
+        # Remove the empty web_static directory
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(file_name))
+
+        # Delete the symbolic link /data/web_static/current
+        run('rm -rf /data/web_static/current')
+
+        # Create a new symbolic link
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(file_name))
+
         return True
-    except Exception as e:
+    except Exception:
         return False
